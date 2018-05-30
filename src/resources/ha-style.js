27475bc302c75c593e20bce569a50e7d73e9cfdb
Andrzej <araczkowski@gmail.com>
2018-05-30 21:59:07 +0200

    public ais_dom files

--- src/resources/ha-style.js
+++ src/resources/ha-style.js
@@ -24,7 +24,7 @@ documentContainer.innerHTML = `<custom-style>
       --disabled-text-color: #bdbdbd;
 
       /* main interface colors */
-      --primary-color: #03a9f4;
+      --primary-color: #FF9800;
       --dark-primary-color: #0288d1;
       --light-primary-color: #b3e5fC;
       --accent-color: #ff9800;
