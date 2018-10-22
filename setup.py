be494ca7dba343a0cfd2459ea3acac0d85f0f58f
Andrzej <araczkowski@gmail.com>
2018-10-22 19:41:01 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='home-assistant-frontend',
+setup(name='ais-dom-frontend',
       version='20181014.0',
-      description='The Home Assistant frontend',
-      url='https://github.com/home-assistant/home-assistant-polymer',
-      author='The Home Assistant Authors',
-      author_email='hello@home-assistant.io',
+      description='The AIS dom frontend',
+      url='https://github.com/sviete/AIS-home-assistant-polymer',
+      author='Amdrzej Raczkowski based on Home Assistant Polymer',
+      author_email='info@sviete.pl',
       license='Apache License 2.0',
       packages=find_packages(include=[
           'hass_frontend',
