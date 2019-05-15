21af56a134b5987b7c98c5145874ded82ffa3770
araczkowski <araczkowski@gmail.com>
2019-05-15 16:10:15 +0200

    new flow for grunt

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='ais-dom-frontend',
-      version='20190514.0',
-      description='AIS dom frontend',
-      url='https://github.com/sviete/home-assistant/home-assistant-polymer',
-      author='Andrzej Raczkowski',
-      author_email='info@sviete.pl',
-      license='Apache License 2.0',
-      packages=find_packages(include=["hass_frontend", "hass_frontend.*"]),
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name="ais-dom-frontend",
+    version="20190514.0",
+    description="AIS dom frontend",
+    url="https://github.com/sviete/home-assistant/home-assistant-polymer",
+    author="Andrzej Raczkowski",
+    author_email="info@sviete.pl",
+    license="Apache License 2.0",
+    packages=find_packages(include=["hass_frontend", "hass_frontend.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
