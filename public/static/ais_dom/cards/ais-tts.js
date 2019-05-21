e018e936200f454c79de4faebc9f0d8d57f8f172
araczkowski <araczkowski@gmail.com>
2019-05-21 19:22:11 +0200

    ais-tts 2

--- public/static/ais_dom/cards/ais-tts.js
+++ public/static/ais_dom/cards/ais-tts.js
@@ -1,9 +1,8 @@
-// TODO  https://www.chromestatus.com/feature/5687444770914304
 let hass = document.querySelector("home-assistant").hass;
 hass.connection.subscribeEvents((event) => {
   if (
     event.data.domain === "ais_ai_service" &&
-    event.data.service === "say_it"
+    event.data.service === "say_in_browser"
   ) {
     if ("speechSynthesis" in window) {
       var speech = new SpeechSynthesisUtterance(event.data.service_data.text);
