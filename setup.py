dc12a7f3b4e46cbf5da09a224947b38dc63bf93b
araczkowski <araczkowski@gmail.com>
2019-05-22 09:58:29 +0200

    swipe navigation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ais-dom-frontend",
-    version="20190514.1",
+    version="20190514.2",
     description="AIS dom frontend",
     url="https://github.com/sviete/home-assistant/home-assistant-polymer",
     author="Andrzej Raczkowski",
