a5933281587a54a502fda0153777b0a14e5a8df3
araczkowski <araczkowski@gmail.com>
2019-05-24 17:14:39 +0200

    sayInFrame

--- public/static/ais_dom/cards/ais-tts.js
+++ public/static/ais_dom/cards/ais-tts.js
@@ -5,9 +5,14 @@ hass.connection.subscribeEvents((event) => {
     event.data.service === "say_in_browser"
   ) {
     if ("speechSynthesis" in window) {
+      window.speechSynthesis.cancel();
       var speech = new SpeechSynthesisUtterance(event.data.service_data.text);
       speech.lang = "pl-PL";
       window.speechSynthesis.speak(speech);
     }
+    // say in frame
+    try {
+      window.JavascriptHandler.sayInFrame(event.data.service_data.text);
+    } catch (err) {}
   }
 }, "call_service");
