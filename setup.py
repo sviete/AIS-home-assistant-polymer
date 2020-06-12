c85a0f03e45088aa0288ea10ecd6fbd47a61dbdb
tomek@sviete.pl <tomek@sviete.pl>
2020-06-12 10:55:34 +0200

    20200603.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ais-dom-frontend",
-    version="20200603.2",
+    version="20200603.3",
     description="AIS dom frontend",
     url="https://github.com/sviete/home-assistant/home-assistant-polymer",
     author="The Authors of the AI-Speaker.com project.",
