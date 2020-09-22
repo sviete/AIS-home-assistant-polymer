357a516135888950e345f3d4309b301f061f1cfc
tomasz75 <tomek@sviete.pl>
2020-09-22 17:44:12 +0200

    ais-play-media dialog

---
+++ src/panels/media-browser/show-media-player-ais-dialog.ts
@@ -0,0 +1,21 @@
+import { fireEvent } from "../../common/dom/fire_event";
+
+export interface WebBrowserPlayMediaAisDialogParams {
+  sourceUrl: string;
+  sourceType: string;
+  title?: string;
+}
+
+export const showWebBrowserPlayMediaAisDialog = (
+  element: HTMLElement,
+  webBrowserPlayMediaAisDialogParams: WebBrowserPlayMediaAisDialogParams
+): void => {
+  fireEvent(element, "show-dialog", {
+    dialogTag: "hui-dialog-web-browser-ais-play-media",
+    dialogImport: () =>
+      import(
+        /* webpackChunkName: "hui-dialog-ais-media-player" */ "./hui-dialog-web-browser-ais-play-media"
+      ),
+    dialogParams: webBrowserPlayMediaAisDialogParams,
+  });
+};
