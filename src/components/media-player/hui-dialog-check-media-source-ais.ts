5857548462e1adf8fbb52a68a3095cbef3600aa0
tomasz75 <tomek@sviete.pl>
2020-09-24 20:59:24 +0200

    ais_404

--- src/components/media-player/hui-dialog-check-media-source-ais.ts
+++ src/components/media-player/hui-dialog-check-media-source-ais.ts
@@ -1,3 +1,4 @@
+import "@material/mwc-button/mwc-button";
 import "@material/mwc-list/mwc-list";
 import "@material/mwc-list/mwc-list-item";
 import "@polymer/paper-item/paper-item";
@@ -48,26 +49,37 @@ export class HuiDialogCheckMediaSourceAis extends LitElement {
       <ha-dialog
         open
         hideActions
-        .heading=${createCloseHeading(this.hass, "Sprawdzenie zródła audio")}
+        .heading=${createCloseHeading(
+          this.hass,
+          "Sprawdzenie źródła multimediów"
+        )}
         @closed=${this.closeDialog}
       >
-      <h3>Obecnie odtwarzasz ze adresu: </h3>
-       ${this._aisMediaInfo?.attributes["media_content_id"]} 
-      <p>Jeżeli jest problem z tym zasobem, to możesz automatycznie sprawdzić, czy jest dostępne bardziej aktualne źródło tego audio.</p>
-         <div class="sourceCheckButton">
+        <div class="img404">
+          <img src="/static/ais_404.png" />
+        </div>
+        ${this._canSourceBeChecked()
+          ? html`<h3>Obecnie odtwarzasz ${this._aisMediaInfo?.attributes["media_title"]}, ze adresu: </h3>
+       <ha-icon icon="mdi:web"></ha-icon> ${this._aisMediaInfo?.attributes["media_content_id"]} 
+      <p>Jeżeli jest problem z tym zasobem, to możesz automatycznie sprawdzić, czy jest dostępne bardziej aktualne źródło:</p>
+        <div class="sourceCheckButton">
           <mwc-button raised @click=${this._handleSourceCheck}>
                     <ha-icon icon="hass:robot"></ha-icon>
-                    Uruchom Automatyczne Sprawdzanie
+                    &nbsp; Uruchom Automatyczne Sprawdzanie
           </mwc-button>
         </div> 
-      <p></p>Jeżeli automatyczne sprawdzenie nie pomoże, to będzie można wysłać informację o nie działającym zasobie do AI-Speaker.</p>
-
+      <p></p>Jeżeli automatyczne sprawdzenie nie pomoże, to będzie można wysłać informację o nie działającym zasobie do AI-Speaker.</p>`
+          : html``}
       </ha-dialog>
     `;
   }
 
   private _handleSourceCheck(): void {
     this.closeDialog();
+  }
+
+  private _canSourceBeChecked(): boolean {
+    return true;
   }
 
   static get styles(): CSSResult[] {
@@ -79,6 +91,12 @@ export class HuiDialogCheckMediaSourceAis extends LitElement {
         }
         div.sourceCheckButton {
           text-align: center;
+        }
+        div.img404 {
+          text-align: center;
+        }
+        img {
+          max-width: 500px;
         }
       `,
     ];
