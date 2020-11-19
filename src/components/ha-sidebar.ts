24e1eaf20931f9e8dce36d486ff4a8d5c6dc9cea
tomasz75 <tomek@sviete.pl>
2020-11-19 08:41:36 +0100

    20201111.3

--- src/components/ha-sidebar.ts
+++ src/components/ha-sidebar.ts
@@ -335,7 +335,7 @@ class HaSidebar extends LitElement {
         ? html`<mwc-button outlined @click=${this._closeEditMode}>
             ${this.hass.localize("ui.sidebar.done")}
           </mwc-button>`
-        : html`<div class="title">Home Assistant</div>`}
+        : html`<div class="title">Asystent domowy</div>`}
     </div>`;
   }
 
